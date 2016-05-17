b02713a505984749979b9c6b0fb425ec558f1784
thiagotmb <tmb0710@gmail.com>
2016-05-16 21:37:37 -0300

    Setting enviroment production

--- ido-service/db/migrate/20160515172055_create_category_enterprises.rb
+++ ido-service/db/migrate/20160515172055_create_category_enterprises.rb
@@ -2,7 +2,7 @@ class CreateCategoryEnterprises < ActiveRecord::Migration[5.0]
   def change
     create_table :category_enterprises do |t|
       t.references :category, foreign_key: true
-      t.references :enterprise, foreign_key: true
+      t.references :enterprise
       t.float :base_price
       t.string :description
       t.integer :capacity
--- ido-service/db/migrate/20160515172057_create_pictures.rb
+++ ido-service/db/migrate/20160515172057_create_pictures.rb
@@ -2,9 +2,9 @@ class CreatePictures < ActiveRecord::Migration[5.0]
   def change
     create_table :pictures do |t|
       t.string :url
-      t.references :user, foreign_key: true
-      t.references :category_enterprise, foreign_key: true
-      t.references :message, foreign_key: true
+      t.references :user
+      t.references :category_enterprise
+      t.references :message
 
       t.timestamps
     end
