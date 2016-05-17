c049b4a241f4e9d81a0b7455152aca2120c7fa6d
thiagotmb <tmb0710@gmail.com>
2016-05-16 21:35:06 -0300

    Setting enviroment production

--- ido-service/db/migrate/20160515172052_create_category_preferences.rb
+++ ido-service/db/migrate/20160515172052_create_category_preferences.rb
@@ -3,8 +3,7 @@ class CreateCategoryPreferences < ActiveRecord::Migration[5.0]
     create_table :category_preferences do |t|
       t.float :budget
       t.references :preference, foreign_key: true
-      t.references :category, foreign_key: true
-
+      t.references :category
       t.timestamps
     end
   end
